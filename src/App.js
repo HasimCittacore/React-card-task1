ab30b1a7b1c65290a1085fa1eaf7e044aad6a165
HasimNewCoder <462mohammed@gmail.com>
2022-04-18 14:03:04 +0530

    first commit of React Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Cards from './components/Cards';
 function App() {
+  let data = [
+    {
+      products:"FREE",
+      price:0,
+      users:"Single User",
+      userEnabler:true,
+      storage:"5GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"CommunityAccess",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:false,
+      phoneSupport:"Dedicated Phone Support",
+      phoneSupportEnabler:false,
+      subDomain:"Free SubDomain",
+      subDomainEnabler:false,
+      reports:"Monthly Status Reports",
+      reportsEnabler:false
+    },
+    {
+      products:"PLUS",
+      price:9,
+      users:"5 Users",
+      userEnabler:true,
+      storage:"50GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"CommunityAccess",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      phoneSupport:"Dedicated Phone Support",
+      phoneSupportEnabler:true,
+      subDomain:"Free SubDomain",
+      subDomainEnabler:true,
+      reports:"Monthly Status Reports",
+      reportsEnabler:false
+    },
+    {
+      products:"PRO",
+      price:49,
+      users:"Unlimited User",
+      userEnabler:true,
+      storage:"150GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"CommunityAccess",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      phoneSupport:"Unlimited Phone Support",
+      phoneSupportEnabler:true,
+      subDomain:"Unlimited Free SubDomain",
+      subDomainEnabler:true,
+      reports:"Monthly Status Reports",
+      reportsEnabler:false
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <section className='card_box py-5'>
+      
+    <div className='container'>
+      <div className='row'>
+        {
+          data.map((e)=>{
+            return <Cards data={e}/>
+          })
+        }
+      </div>
     </div>
+    </section>
+    </>
   );
 }
 
